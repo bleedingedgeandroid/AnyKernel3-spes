1dd36098527f4ff3786aced9c15b12ea82b58a5c
PugzAreCute <me@pugzarecute.com>
2024-01-07 14:52:04 +0530

    Forget variables

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 
 properties() { '
-kernel.string=spes/spesn Kernel. Version $KVERSION. CI build: $CI_BUILDNO-$CI_BUILDSUFFIX  | Source by @muralivijay9845 | Compiled by @PugzAreCute
+kernel.string=Murali680 spes/spesn Kernel. Version INTERNAL_KVERSION. CI build: CIBUILD | Source by @muralivijay9845 | Compiled by @PugzAreCute
 do.devicecheck=1
 do.modules=0
 do.systemless=0
